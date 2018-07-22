de68808c14fe049d6b8088586b478850d45cb215
Simon Pringle-Wallace <sandscape145@MacBook-Pro.local>
2018-07-21 22:51:28 -0400

    get century kata solved for real

--- get_century.js
+++ get_century.js
@@ -1,17 +1,7 @@
 var century = function(x){
-  if (x <= 100) {
-  	return (1);
-} else if (x <1000) {
-  	var centuryString = String(x);
-  	var first = centuryString[0];
-  	firstInt = parseInt(first);
-  	return(firstInt+1);
-} else if (x < 10000) {
-	var centuryString = String(x);
-	var firstTwo = centuryString[0]+centuryString[1];
-    firstTwoInt = parseInt(firstTwo);
-    return(firstTwoInt+1);
+  	var century = (x / 100);
+    return (Math.ceil(century));
 }
-}
+
 
 
